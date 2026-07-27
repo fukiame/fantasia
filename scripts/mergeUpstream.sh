b3e5a4b9021fc95601eddd202f8099fe1ee82da2
fukiame <fukiame@proton.me>
2026-07-27 10:39:35 +0700

    update upstream (Flick)
    Signed-off-by: fukiame <fukiame@proton.me>

--- scripts/mergeUpstream.sh
+++ scripts/mergeUpstream.sh
@@ -5,12 +5,12 @@ basedir=`pwd`
 
 function update {
     cd "$basedir/$1"
-    git fetch && git reset --hard origin/master
+    git fetch && git reset --hard "origin/$2"
     cd "$basedir/$1/.."
-    git add $1
+    git add "$1"
 }
 
-update $1
+update "$1" "$2"
 
 # Update submodules
 git submodule update --recursive
